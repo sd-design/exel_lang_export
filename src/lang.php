212097e30f945527082ccc04d57a07c6d164afc5
Alex Solovey <alex@sd-design.tk>
2021-06-28 15:33:11 +0300

    Update lang.php

--- src/lang.php
+++ src/lang.php
@@ -3,6 +3,6 @@
 $langArray = [
     'head_title' => 'Водородный генератор DEAQUA',
     'header_title' => 'Иновация оздоровления - водородный генератор DEAQUA',
-    'header_description' => 'водородный генератор DEAQUA - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium reprehenderit doloribus repellat, eius libero suscipit cum consequuntur veniam. Possimus suscipit aliquid fugiat ipsam eligendi quidem quae enim blanditiis accusantium maiores.',
+    'header_description' => 'водородный генератор DEAQUA - Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     'subtitle_1' => 'Hello world!!!'
 ];
